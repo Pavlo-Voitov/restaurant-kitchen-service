7154186282e2cf3386d277e907b63fa7280cd40e
Pavlo <vojtovp20@gmail.com>
2026-02-07 13:43:23 +0200

    Create templatetags and fix/update pagination

---
+++ kitchen/templatetags/query_string.py
@@ -0,0 +1,14 @@
+from django import template
+
+
+register = template.Library()
+
+@register.simple_tag
+def query_string(request, **kwargs):
+    params = request.GET.copy()
+    if "page" in params:
+        params.pop("page")
+
+    for k, v in kwargs.items():
+        params[k] = v
+    return params.urlencode()
